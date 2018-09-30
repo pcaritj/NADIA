631f7f53abf116e0f4e823e92e1d3fbece2b71fb
Paul Caritj <caritj@gmail.com>
2018-09-29 21:46:11 -0400

    Restructure for use with pip
    Former-commit-id: 0178e16383faf32c3913fe99c53503264cddb77c

--- nadia_node/setup.py
+++ nadia_node/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='nadia',
+setup(name='nadia_node',
       version='0.1',
       description='The Nanoscale Distributed Interferometric Array',
       url='https://github.com/pcaritj/NADIA',
       author='',
       author_email='',
       license='MIT',
-      packages=['nadia'],
+      packages=['nadia_node'],
       zip_safe=False)
