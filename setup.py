380d2d1c754f31ddb1175edb94632e20826ec8d5
Paul Caritj <caritj@gmail.com>
2018-09-29 21:37:43 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nadia',
+      version='0.1',
+      description='The Nanoscale Distributed Interferometric Array',
+      url='https://github.com/pcaritj/NADIA',
+      author='',
+      author_email='',
+      license='MIT',
+      packages=['nadia'],
+      zip_safe=False)
